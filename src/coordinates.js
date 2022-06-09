a90bb36dbf4dc04916e30b3e77878fe397440083
IhorKozloff <ik.eyeshield21@gmail.com>
2022-06-09 21:59:19 +0300

    99% html and css done

---
+++ src/coordinates.js
@@ -0,0 +1,17 @@
+// +   +46 10 - 30 19 Belgorod Dnestrovskiy +49 54 - 28 39 Berdichev +46 45 - 36 47 
+// Berdyansk +49 27 - 324 Cherkassy +51 30 - 31 18 Chernigov +51 17 - 30 15 Chernobyl +48 19 - 25 52 Chernovtsy +48 30 - 34 37 
+// Dneprodzerzhinsk +48 29 - 350 Dnepropetrovsk +480 - 37 50 Donetsk +453 - 35 23 Feodosiya +48 17 - 385 Gorlovka +44 33 - 34 15 
+// Gurzuf +48 40 - 24 40 Ivano-Frankovsk +475 - 37 34 Jdanov +48 40 - 26 36 Kamenets Podolskiy +45 22 - 36 27 Kerch +500 - 36 15 
+// Kharkov +46 39 - 32 38 Kherson +49 25 - 26 59 Khmelnitskiy +50 25 - 30 30 Kiev +48 31 - 32 15 Kirovograd +51 12 - 24 48 
+// Kovel +48 43 - 37 33 Kramatorsk +493 - 33 25 Kremenchug +47 55 - 33 24 Krivoy Rog +50 42 - 25 15 Lutsk +51 20 - 25 53 
+// Kuznetsovsk +49 50 - 240 Lvov +481 - 380 Makeyevka +46 51 - 35 22 Melitopol +48 26 - 22 45 Mukachevo +46 57 - 320 Nikolayev +47 34 - 34 25
+//  Nikopol +46 30 - 30 46 Odessa +49 35 - 34 35 Poltava +50 39 - 26 10 Rovno +44 36 - 33 31 Sebastopol +48 08 - 37 18 Selidovo +48 58 - 38 29 
+//  Severodonetsk +44 57 - 345 Simferopol +48 54 - 37 33 Slavyansk +50 55 - 34 49 Sumy +49 35 - 25 39 Ternopol +48 45 - 30 10 Uman +48 38 - 22 15 
+//  Uzhgorod +49 11 - 28 30 Vinnitsa +48 35 - 39 20 
+// Voroshilovgrad +44 30 - 349 Yalta +47 50 - 35 10 Zaporozhye +50 18 - 28 40 Zhitomir 
+const template = {nameEn:"", nameUA: "", nameRu: "", latitude:"", longtitude:""}
+const cityGeographicalCoordinates = [
+    {nameEn:"Alexandria", nameUA: "Александрія", nameRu: "", latitude:"48.41", longtitude:"-335"},
+];
+export default AlexandriaGeographicalCoordinates =
+    {nameEn:"Alexandria", nameUA: "Александрія", nameRu: "Александрия", latitude:"48.41", longtitude:"-335"};
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import AlexandriaGeographicalCoordinates from './coordinates';
+import axios from "axios"
+
+
+const apiKey = "cf43e0136f8c34fc07ed8b788e0c7b14"
+const BASE_URL = `https://api.openweathermap.org/data/3.0/onecall`
+
+
+
+const fetchUrl = `${BASE_URL}?lat=${AlexandriaGeographicalCoordinates.latitude}&lon=${AlexandriaGeographicalCoordinates.longtitude}&appid=cf43e0136f8c34fc07ed8b788e0c7b14`
+console.log(fetchUrl)
+
+
+async function getWeatherByName () {
+
+    const response = await axios.get(fetchUrl)
+    console.log(response.data)
+}
+
+getWeatherByName();
